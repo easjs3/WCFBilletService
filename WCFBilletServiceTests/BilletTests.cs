640e1b13b88c4db7afdefdd1e184c3205322ab78
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-09-20 09:46:17 +0200

    unit test lavet mangler krop

---
+++ WCFBilletServiceTests/BilletTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WCFBilletService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFBilletService.Tests
+{
+    [TestClass()]
+    public class BilletTests
+    {
+        [TestMethod()]
+        public void BilPrisUdenRabatStorebæltTest()
+        {
+           //arrange
+           //act
+           //assert
+        }
+
+        [TestMethod()]
+        public void MCPrisUdenRabatStorebæltTest()
+        {
+            //arrange
+            //act
+            //assert
+        }
+
+        [TestMethod()]
+        public void BilPrisUdenRabatØresundTest()
+        {
+            //arrange
+            //act
+            //assert
+        }
+
+        [TestMethod()]
+        public void MCPrisUdenRabatØresundTest()
+        {
+            //arrange
+            //act
+            //assert
+        }
+    }
+}
