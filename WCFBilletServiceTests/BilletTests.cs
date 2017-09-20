b9f3e0c598666d79530c26671c6cecc8c45892de
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-09-20 09:55:30 +0200

    lavet unit tests

--- WCFBilletServiceTests/BilletTests.cs
+++ WCFBilletServiceTests/BilletTests.cs
@@ -2,6 +2,7 @@
 using WCFBilletService;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,33 +15,45 @@ namespace WCFBilletService.Tests
         [TestMethod()]
         public void BilPrisUdenRabatStorebæltTest()
         {
-           //arrange
-           //act
-           //assert
+            //arrange
+            Billet minbillet = new Billet();
+            //act
+            int minint = minbillet.BilPrisUdenRabatStorebælt();
+            //assert
+            Assert.AreEqual(240, minint);
         }
 
         [TestMethod()]
         public void MCPrisUdenRabatStorebæltTest()
         {
             //arrange
+            Billet minbillet = new Billet();
             //act
+            int minint = minbillet.MCPrisUdenRabatStorebælt();
             //assert
+            Assert.AreEqual(125, minint);
         }
 
         [TestMethod()]
         public void BilPrisUdenRabatØresundTest()
         {
             //arrange
+            Billet minbillet = new Billet();
             //act
+            int minint = minbillet.BilPrisUdenRabatØresund();
             //assert
+            Assert.AreEqual(240, minint);
         }
 
         [TestMethod()]
         public void MCPrisUdenRabatØresundTest()
         {
             //arrange
+            Billet minbillet = new Billet();
             //act
+            int minint = minbillet.MCPrisUdenRabatØresund();
             //assert
+            Assert.AreEqual(125, minint);
         }
     }
 }
